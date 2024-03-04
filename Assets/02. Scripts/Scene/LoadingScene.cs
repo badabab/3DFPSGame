03f3728ddbd32a4ce8470d132f1155d0981eeb7d
badabab <ky040723@gmail.com>
2024-03-04 15:12:39 +0900

    LoadingScene

---
+++ Assets/02. Scripts/Scene/LoadingScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScene : MonoBehaviour
+{
+    public SceneNames NextScene;
+    void Start()
+    {
+        LoadNextScene();
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene((int)NextScene);
+    }
+}
